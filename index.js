6e86103047d28b800e256c9777a9bbb5fb31d805
Eyobed Feleke <52031294+eyobed7b@users.noreply.github.com>
2022-05-23 07:18:07 +0000

    some changes made

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 let ejs = require('ejs');
 const haikus = require('./haikus.json');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs');
